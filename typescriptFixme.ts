86b8c1ad17a37878716c6bc542185fb7263b55a2
Rakshit Sain <rakshitsain895@gmail.com>
2024-06-17 19:12:45 -0400

    task done

--- typescriptFixme.ts
+++ typescriptFixme.ts
@@ -1,94 +1,132 @@
 // add the type annotation to explicitly specify the type of the variable:
-var isDone = false;
-var lines = 42;
-var name = "Anders";
+var isDone: boolean = false;
+var lines: number = 42;
+// var name: string = "Anders";
 
 // annotate the type as when it's not known
-let unknownTypeVariable;
+let unknownTypeVariable: any;
 
 //annotate this array
-var list = [1, 2, 3];
+var list : Array<number>= [1, 2, 3];
 
 // implement color in typescript
-var Color = { Red: 0, Green: 1, Blue: 2 };
-var c = Color.Green;
-console.log(Object.keys(Color)[c]); // "Green"
+enum Color { Red = 0, Green = 1, Blue = 2 };
+var c: Color = Color.Green;
+console.log(Color[c]); // "Green"
 
-function littleHorribleAlert() {
+function littleHorribleAlert(): void {
   alert("I am a little annoying box!!");
 }
 
-var f1 = function (i) {
-  return i * i;
-};
-// as f1 just don't specify the return type here
-var f2 = function (i) {
+var f1: (i: number) => number = function (i: number) {
   return i * i;
 };
 
-var f3 = (i) => {
+
+// as f1 just don't specify the return type here
+var f2 = function (i: number) {
+  return i * i;
+};
+
+var f3 = (i: number) => {
   return i * i;
 };
 
 //change f4 to accept a string or a number as a parameter
-function f4(i) {
+function f4(i: string | number) {
   console.log("The value was " + i);
 }
 
 // Annotate the person objects with an interface. Some properties are optional
-var person1 = { name: "Bobby", move: function () {} };
-var person2 = { name: "Bobby", age: 42, move: function () {} };
+interface Person {
+  name: string;
+  age?: number;
+  move: () => void;
+}
+
+var person1: Person = { name: "Bobby", move: function () {} };
+var person2: Person = { name: "Bobby", age: 42, move: function () {} };
+
+
 
 // Annotate mySearch to describe a function that accepts two string parameters
-var mySearch = function (src, sub) {
+var mySearch: (src: string, sub: string) => boolean = function (src, sub) {
   return src.search(sub) != -1;
 };
 
+
 //annotate the constructor arguments and properties
 class Point {
-  x = 0;
-  constructor(x, y = 0) {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number = 0) {
     this.x = x;
     this.y = y;
   }
-  dist() {
+
+  dist(): number {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   }
 }
 
 // annotate `PointPerson` to use all properties of `Person` and then add the missing required property
-class PointPerson {
-  constructor(name) {
-    this.name = name;
-  }
+interface Person {
+  name: string;
+  move: () => void;
 }
+
+class PointPerson implements Person {
+  name: string;
+  age: number; // the missing required property
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  move = () => {};
+}
+
+
 
 // Inheritance
 // Make `Point3D` inherit from `Point`
 // Don't forget to call the parent constructor
-class Point3D {
-  constructor(x, y, z = 0) {
+class Point3D extends Point {
+  z: number;
+
+  constructor(x: number, y: number, z: number = 0) {
+    super(x, y);
     this.z = z;
   }
-  dist() {
+
+  dist(): number {
     var d = super.dist();
     return Math.sqrt(d * d + this.z * this.z);
   }
 }
 
 // Make `Tuple` Generic that it accepts to generic types as arguments
+class Tuple<T1, T2> {
+  item1: T1;
+  item2: T2;
 
-class Tuple {
-  constructor(item1, item2) {
+  constructor(item1: T1, item2: T2) {
     this.item1 = item1;
     this.item2 = item2;
   }
 }
 
 // create a generic interface called Pair that accepts one generic type for two properties named item1 and item2
+interface Pair<T> {
+  item1: T;
+  item2: T;
+}
+
 
 // create a function that will take a pair with a generic argument and return a `Tuple`
-function pairToTuple(pair) {
+function pairToTuple<T>(pair: Pair<T>): Tuple<T, T> {
   return new Tuple(pair.item1, pair.item2);
 }
 
@@ -96,9 +134,9 @@ var tuple = pairToTuple({ item1: "hello", item2: "world" });
 
 // annotate `make`, `model`, and `year` to be not mutable (read only) outside the constructor
 class Car {
-  make;
-  model;
-  year = 2018;
+  readonly make: string;
+  readonly model: string;
+  readonly year: number = 2018;
 
   constructor() {
     this.make = "Unknown Make";
@@ -106,10 +144,13 @@ class Car {
   }
 }
 
+
 // annotate this array
-var arrayOfAnyType = [1, "string", false];
+var arrayOfAnyType: (number | string | boolean)[] = [1, "string", false];
+
 
 // fix the foo object so bar and baz can be added
-let foo = {};
+let foo: { bar: number; baz: string; } = {bar: 0, baz: ""};
 foo.bar = 123;
 foo.baz = "hello world";
+
